f71c5d99502c07d729ef8c61546f34040b266e3e
rehanheeeeeeeee <rehantosif4@gmail.com>
2023-01-28 15:48:36 +0300

    Update Controllers

--- server/controllers/music.js
+++ server/controllers/music.js
@@ -1,5 +1,6 @@
 const getMuic = (req, res) => {
   try {
+    console.log("Vero");
     res.status(202).json({ success: true });
   } catch (err) {
     res.status(404).json({ error: err.message });
