dd787ad8172ab90a5da698cd0d27fb0d3ca72ed4
rehanheeeeeeeee <rehantosif4@gmail.com>
2023-01-28 16:32:40 +0300

    Deleted controller file for music route

--- server/controllers/music.js
+++
@@ -1,8 +0,0 @@
-const getMuic = (req, res) => {
-  try {
-    console.log("Vero");
-    res.status(202).json({ success: true });
-  } catch (err) {
-    res.status(404).json({ error: err.message });
-  }
-};
